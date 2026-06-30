ff4a8d3d447989d943ab6d5e14b03828c5d0a7e6
rania-9 <150871992+rania-9@users.noreply.github.com>
2026-06-29 19:22:02 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useRef } from 'react';
+import nicotine from './nicotine.png';
+import brain from './brain.png';
+import brainparts from './brainparts.png';
+import brickbrain from './brickbrain.png';
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+function App() {
+    const ref = useRef();
+  
+    return(
+      <div>
+        <Parallax pages={4} ref={ref}>
+          <ParallaxLayer
+            sticky={{ start: 0, end: 1 }}>
+            <img src={brickbrain} />
+          </ParallaxLayer>
+          <ParallaxLayer
+            horizontal={true}
+            >
+            <img src={nicotine} />
+          </ParallaxLayer>
+        </Parallax>
+      </div>
+    )
+};
